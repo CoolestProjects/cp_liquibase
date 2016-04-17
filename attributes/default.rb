9067b19cbbf398a29e0009b745b3b5dfe93cb6d0
Noel King <noelking@gmail.com>
2016-04-17 12:14:23 +0100

    Updated liquibase url

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default['cp_liquibase']['install_dir'] = "/opt/liquibase"
 default['cp_liquibase']['dist_dir'] = "/opt/liquibase/dist"
 default['cp_liquibase']['zip_foldername'] = ""
 
-default['cp_liquibase']['url']="https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.1/liquibase-3.4.1-bin.zip"
+default['cp_liquibase']['url']="https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.2/liquibase-3.4.2-bin.zip"
 default['cp_liquibase']['binary_file_name']="liquibase-3.4.1-bin.zip"
 default['cp_liquibase']['driver_jar_url'] = "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.25/mysql-connector-java-5.1.25.jar"
 default['cp_liquibase']['driver_file_name'] = "mysql-connector-java-5.1.25.jar"
@@ -17,4 +17,3 @@ default['cp_liquibase']['command'] = "update"
 default['cp_liquibase']["sql_binary_url"]=""
 default['cp_liquibase']['dist_local_file_path'] = "/dist/"
 default['cp_liquibase']['use_local_dist'] = "false"
-
